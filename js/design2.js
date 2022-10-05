c0f47a2083d750e264a85a2ce452c5a9eac55cc7
Fahim Ahmed Khan Gorbo <53188460+Fahimahmedkhan@users.noreply.github.com>
2022-10-06 00:44:26 +0600

    first commit

---
+++ js/design2.js
@@ -0,0 +1,8 @@
+const container = document.getElementById('container');
+
+window.onmousemove = function (e) {
+    const x = - e.clientX / 5;
+    const y = - e.clientY / 5;
+    container.style.backgroundPositionX = x + 'px';
+    container.style.backgroundPositionY = y + 'px';
+}
